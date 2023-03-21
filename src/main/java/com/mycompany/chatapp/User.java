8b031ef86e6efe7b916e6f81e2957f95930f1562
Zaliye Prasandike <73892701+Saliya99@users.noreply.github.com>
2023-03-21 10:17:23 +0530

    User.java

--- src/main/java/com/mycompany/chatapp/User.java
+++ src/main/java/com/mycompany/chatapp/User.java
@@ -3,11 +3,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.chatapp;
+import javax.persistence.*;
 
 /**
  *
  * @author Zali
  */
+@Entity
+@Table(name = "users")
 public class User {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private int userId;
+
+    @Column(name = "username")
+    private String username;
+
+    // Constructor, getters, and setters
 }
